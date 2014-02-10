e3c2443a97bdc10ed1bb4568c15957008bfa1081
zyberspace <zyberspace@zyberware.org>
2014-02-10 16:55:24 +0100

    wachhund automatically reconnects, if the connection gets lost
    If no connection is available from start, wachhund will try to connect till he is connected.

--- wachhund.js
+++ wachhund.js
@@ -3,18 +3,30 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-var config = require("./config");
+var TeamspeakClient = require("node-teamspeak"),
+    config = require("./config");
 
-var teamspeak = new (require("node-teamspeak"))(config.host);
-teamspeak.send("login", {client_login_name: config.loginName, client_login_password: config.loginPassword}, function(err, response) {
-    teamspeak.send("use", {sid: config.serverId}, function(err, response) {
-        teamspeak.send("clientupdate", {client_nickname: config.clientName}, function(err, response) {
-            //Add module-dir to the module-paths
-            module.paths.push("modules");
-            //Execute modules
-            for (module in config.modules) {
-                require(module)(teamspeak, config.modules[module]);
-            }
+//Add module-dir to the module-paths
+module.paths.push("modules");
+
+(function wachhund() {
+    var teamspeak = new TeamspeakClient(config.host);
+    teamspeak.send("login", {client_login_name: config.loginName, client_login_password: config.loginPassword}, function(err, response) {
+        teamspeak.send("use", {sid: config.serverId}, function(err, response) {
+            teamspeak.send("clientupdate", {client_nickname: config.clientName}, function(err, response) {
+                //Execute modules
+                for (module in config.modules) {
+                    require(module)(teamspeak, config.modules[module]);
+                }
+            });
         });
     });
-});
+    teamspeak.on("error", function() {}); //node-teamspeak only emits errors, if the socket emits errors,
+                                          //so send them to the blackhole
+    teamspeak.on("close", function() {
+        //Try to reconnect/restart after 3 seconds
+        setTimeout(function() {
+            wachhund();
+        }, 3000);
+    });
+})();
