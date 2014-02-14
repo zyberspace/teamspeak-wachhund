16b9be039830d61d4c47efe530ad44577ff85f92
zyberspace <zyberspace@zyberware.org>
2014-02-14 16:45:35 +0100

    Better config-include
    (for usage with nexe)

--- wachhund.js
+++ wachhund.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 var TeamspeakClient = require("node-teamspeak"),
-    config = require("./config");
+    config = JSON.parse(require("fs").readFileSync("config.json"));
 
 //Add module-dir to the module-paths
 module.paths.push("modules");
